83f788fa23961ce33e3e228e82f8e59c57bf99e2
vivshaw <hvivianshaw@gmail.com>
2019-04-12 01:57:29 -0400

    fix section angled edges in index

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,49 +8,51 @@ import "./index.scss";
 export default () => {
   return (
     <Layout>
-      <section className="section intro-section primary-bg">
-        <div className="intro">
-          <h2 className="is-size-1 has-text-weight-bold has-text-white">
-            Hi! I{`'`}m <em>Hannah Vivian Shaw</em>. I{`'`}m a developer working
-            mainly in <em>Javascript</em> & <em>Java</em>. I{`'`}m into the{" "}
-            <em>React/Redux</em> ecosystem, functional programming, and machine
-            learning.
-          </h2>
-        </div>
-      </section>
+      <div className="slashed-container">
+        <section className="section intro-section rslash-section has-background-primary">
+          <div className="section-content">
+            <h2 className="is-size-1 has-text-weight-bold has-text-white">
+              Hi! I{`'`}m <em>Hannah Vivian Shaw</em>. I{`'`}m a developer
+              working mainly in <em>Javascript</em> & <em>Java</em>. I{`'`}m
+              into the <em>React/Redux</em> ecosystem, functional programming,
+              and machine learning.
+            </h2>
+          </div>
+        </section>
 
-      <section className="section second-section topo-bg">
-        <div className="intro">
-          <h2 className="is-size-1 has-text-weight-bold">
-            Hi! I{`'`}m <em>Hannah Vivian Shaw</em>. I{`'`}m a developer working
-            mainly in <em>Javascript</em> & <em>Java</em>. I{`'`}m into the{" "}
-            <em>React/Redux</em> ecosystem, functional programming, and machine
-            learning.
-          </h2>
-        </div>
-      </section>
+        <section className="section lslash-section has-background-topoteal">
+          <div className="section-content">
+            <h2 className="is-size-1 has-text-weight-bold">
+              Hi! I{`'`}m <em>Hannah Vivian Shaw</em>. I{`'`}m a developer
+              working mainly in <em>Javascript</em> & <em>Java</em>. I{`'`}m
+              into the <em>React/Redux</em> ecosystem, functional programming,
+              and machine learning.
+            </h2>
+          </div>
+        </section>
 
-      <section className="section third-section primary-bg">
-        <div className="intro">
-          <h2 className="is-size-1 has-text-weight-bold has-text-white">
-            Hi! I{`'`}m <em>Hannah Vivian Shaw</em>. I{`'`}m a developer working
-            mainly in <em>Javascript</em> & <em>Java</em>. I{`'`}m into the{" "}
-            <em>React/Redux</em> ecosystem, functional programming, and machine
-            learning.
-          </h2>
-        </div>
-      </section>
+        <section className="section third-section rslash-section has-background-primary">
+          <div className="section-content">
+            <h2 className="is-size-1 has-text-weight-bold has-text-white">
+              Hi! I{`'`}m <em>Hannah Vivian Shaw</em>. I{`'`}m a developer
+              working mainly in <em>Javascript</em> & <em>Java</em>. I{`'`}m
+              into the <em>React/Redux</em> ecosystem, functional programming,
+              and machine learning.
+            </h2>
+          </div>
+        </section>
 
-      <section className="section second-section square-bg">
-        <div className="intro">
-          <h2 className="is-size-1 has-text-weight-bold">
-            Hi! I{`'`}m <em>Hannah Vivian Shaw</em>. I{`'`}m a developer working
-            mainly in <em>Javascript</em> & <em>Java</em>. I{`'`}m into the{" "}
-            <em>React/Redux</em> ecosystem, functional programming, and machine
-            learning.
-          </h2>
-        </div>
-      </section>
+        <section className="section lslash-section has-background-squareyellow">
+          <div className="section-content">
+            <h2 className="is-size-1 has-text-weight-bold">
+              Hi! I{`'`}m <em>Hannah Vivian Shaw</em>. I{`'`}m a developer
+              working mainly in <em>Javascript</em> & <em>Java</em>. I{`'`}m
+              into the <em>React/Redux</em> ecosystem, functional programming,
+              and machine learning.
+            </h2>
+          </div>
+        </section>
+      </div>
     </Layout>
   );
 };
