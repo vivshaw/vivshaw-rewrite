f751c37d39aae3f84b48cb5a0678598bc860efb2
vivshaw <hvivianshaw@gmail.com>
2019-07-30 01:45:30 -0400

    fix sitemap and add rss feed

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `vivshaw`,
-    description: `vivshaw blog`,
-    author: `vivshaw`,
-    siteUrl: `https://vivshaw.github.io`,
+    title: `vivshaw.net`,
+    description: `Hannah Vivian Shaw's blog`,
+    author: `Hannah Vivian Shaw`,
+    siteUrl: `https://vivshaw.net`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -107,6 +107,65 @@ module.exports = {
           Softwares: `Visual Studio Code`,
         },
         note: `Built with love on the JAMStack.`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-feed`,
+      options: {
+        query: `
+          {
+            site {
+              siteMetadata {
+                title
+                description
+                siteUrl
+                site_url: siteUrl
+              }
+            }
+          }
+        `,
+        feeds: [
+          {
+            serialize: ({ query: { site, allMarkdownRemark } }) => {
+              return allMarkdownRemark.edges.map(edge => {
+                return Object.assign({}, edge.node.frontmatter, {
+                  description: edge.node.excerpt,
+                  date: edge.node.frontmatter.date,
+                  url: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                  guid: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                  custom_elements: [{ "content:encoded": edge.node.html }],
+                });
+              });
+            },
+            query: `
+              {
+                allMarkdownRemark(
+                  filter: { fileAbsolutePath: { regex: "/blog/" } }
+                  sort: { order: DESC, fields: [frontmatter___date] },
+                ) {
+                  edges {
+                    node {
+                      excerpt
+                      html
+                      fields { slug }
+                      frontmatter {
+                        title
+                        date
+                      }
+                    }
+                  }
+                }
+              }
+            `,
+            output: "/rss.xml",
+            title: "vivshaw.net RSS Feed",
+            // optional configuration to insert feed reference in pages:
+            // if `string` is used, it will be used to create RegExp and then test if pathname of
+            // current page satisfied this regular expression;
+            // if not provided or `undefined`, all pages will have feed reference inserted
+            match: "^/blog/",
+          },
+        ],
       },
     },
   ],
