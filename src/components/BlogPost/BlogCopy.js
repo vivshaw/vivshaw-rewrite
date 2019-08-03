84a62141974575ebbf540f3835c7fa3561ed02b2
vivshaw <hvivianshaw@gmail.com>
2019-08-03 00:32:53 -0400

    continue tweaking header for better semantics and mobile look

--- src/components/BlogPost/BlogCopy.js
+++ src/components/BlogPost/BlogCopy.js
@@ -1,21 +1,28 @@
-import styled from "styled-components";
+import React from "react";
+import { Box } from "rebass";
 
-const BlogCopy = styled.article`
-  font-family: "Spectral";
+const BlogCopy = props => (
+  <Box
+    {...props}
+    css={`
+      font-family: "Spectral";
 
-  p {
-    font-size: 18px;
-    letter-spacing: -0.72;
-    line-height: 28px;
-    margin-bottom: 28px;
-  }
+      p {
+        font-size: 18px;
+        letter-spacing: -0.72;
+        line-height: 28px;
+        margin-bottom: 28px;
+      }
 
-  h2 {
-    font-weight: 500;
-    font-family: "Rubik";
-    font-size: 24px;
-    margin-bottom: 10px;
-  }
-`;
+      h2 {
+        font-weight: 500;
+        font-family: "Rubik";
+        font-size: 24px;
+        margin-bottom: 10px;
+      }
+    `}
+    px={2}
+  />
+);
 
 export default BlogCopy;
--- src/components/BlogPost/BlogHeader.js
+++ src/components/BlogPost/BlogHeader.js
@@ -5,66 +5,66 @@ import { Box, Text, Flex } from "rebass";
 
 const BlogHeader = ({ title, blurb, date, image, timeToRead }) => {
   return (
-    <Box>
+    <Box as="header">
       <Img fluid={image.childImageSharp.fluid} alt="" />
 
-      <Text
-        as="h1"
-        fontFamily="serif"
-        fontWeight={400}
-        fontSize={"36px"}
-        lineHeight={"44px"}
-        mt={3}
-        mx={"4vw"}
-      >
-        {title}
-      </Text>
-
-      {blurb && (
+      <Box px={2}>
         <Text
-          as="h2"
-          fontFamily="sans"
-          fontSize={4}
-          fontWeight={300}
-          color="grey.6"
-          mx={"4vw"}
-          mt={1}
+          as="h1"
+          fontFamily="serif"
+          fontWeight={400}
+          fontSize={6}
+          lineHeight={"44px"}
+          mt={3}
         >
-          {blurb}
+          {title}
         </Text>
-      )}
 
-      <Flex mx={"4vw"} my={4} flexDirection="row" alignItems="center">
-        <Box
-          css={`
-            height: 48px;
-            border-radius: 50%;
-          `}
-          backgroundColor="red"
-          width={"48px"}
-          mr={2}
-        ></Box>
-        <Box>
+        {blurb && (
           <Text
+            as="h2"
             fontFamily="sans"
-            fontWeight="normal"
-            fontSize={1}
-            as="h6"
-            mb={1}
-          >
-            Hannah Vivian Shaw
-          </Text>
-          <Text
-            as="h6"
-            fontFamily="sans"
-            fontWeight="normal"
-            fontSize={1}
+            fontSize={4}
+            fontWeight={300}
             color="grey.6"
+            mt={1}
           >
-            {date} · {timeToRead} min read
+            {blurb}
           </Text>
-        </Box>
-      </Flex>
+        )}
+
+        <Flex my={4} flexDirection="row" alignItems="center">
+          <Box
+            css={`
+              height: 48px;
+              border-radius: 50%;
+            `}
+            backgroundColor="red"
+            width={"48px"}
+            mr={2}
+          ></Box>
+          <Box>
+            <Text
+              fontFamily="sans"
+              fontWeight="normal"
+              fontSize={1}
+              as="h6"
+              mb={1}
+            >
+              Hannah Vivian Shaw
+            </Text>
+            <Text
+              as="h6"
+              fontFamily="sans"
+              fontWeight="normal"
+              fontSize={1}
+              color="grey.6"
+            >
+              {date} · {timeToRead} min read
+            </Text>
+          </Box>
+        </Flex>
+      </Box>
     </Box>
   );
 };
