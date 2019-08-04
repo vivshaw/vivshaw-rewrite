c67a33f9802faf3df86d7eb031ced020e619a8cd
vivshaw <hvivianshaw@gmail.com>
2019-08-04 01:21:40 -0400

    change typography

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -21,9 +21,11 @@ export default {
     bg: "#f7f7f3",
   },
   fonts: {
-    sans: "Rubik",
-    serif: "Spectral",
-    mono: "IBM Plex Mono",
+    sans: `"Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI",
+    "Oxygen", "Ubuntu", "Helvetica Neue", Arial, sans-serif, "Apple Color Emoji",
+    "Segoe UI Emoji", "Segoe UI Symbol"`,
+    serif: `"Libre Baskerville", Baskerville, Georgia, serif`,
+    mono: `"IBM Plex Mono", SFMono-Regular, Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace`,
   },
   pageWidth: "55vw",
 };
